e840dbdb87bffcb81c805b7fafb8827f2c9be78a
Valentyna Levkovska <78691453+vlevkovska@users.noreply.github.com>
2021-06-28 14:26:07 +0300

    Task 2 finished

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
-const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
-];
+const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
+const elements = [];
+for (let i = 0; i < ingredients.length; i += 1) {
+  const option = ingredients[i];
+  const itemEl = document.createElement('li');
+  itemEl.textContent = option;
+  const listEl = document.querySelector('#ingredients');
+  listEl.appendChild(itemEl);
+  elements.push(itemEl);
+}
+console.log(elements);
+
+// const elements = ingredients.map(option => {
+//   const itemEl = document.createElement('li');
+//   itemEl.textContent = option;
+//   const listEl = document.querySelector('#ingredients');
+//   listEl.appendChild(itemEl);
+//   return itemEl;
+// });
+// console.log(elements);
